343672c828e8878729ea13205da9154c69810cd4
Mateusz Stefanowicz <mt.stefanowicz@gmail.com>
2026-01-09 18:41:14 +0100

    fix: import

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ import logging
 
 from constants import *
 from bible_lookup import get_bible_text
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from typing import List
 
 # Initialize LLMs
